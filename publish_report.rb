cdb124fb14b658c4ea188bc8be4407daa6ba5967
romanrizzi <rizziromanalejandro@gmail.com>
2020-09-11 15:11:50 -0300

    DEV: Add script to submit report

--- publish_report.rb
+++ publish_report.rb
@@ -6,17 +6,16 @@ require_relative 'lib/printers/markdown_printer.rb'
 require_relative 'lib/archives/file_system_archive.rb'
 require 'discourse_api'
 
-report_path = ARGV[0]
-discourse_url = ARGV[1]
-api_key = ARGV[2]
-username = ARGV[3]
-topic_id = ARGV[4]
+discourse_url = ARGV[0]
+api_key = ARGV[1]
+username = ARGV[2]
+topic_id = ARGV[3]
 
 client = DiscourseApi::Client.new(discourse_url)
 client.api_key = api_key
 client.api_username = username
 
-working_dir = File.expand_path(report_path, __FILE__)
+working_dir = File.expand_path('../reports', __FILE__)
 archive = FileSystemArchive.new(working_dir, 'build_report.json')
 threshold = 3
 
